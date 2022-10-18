63648c2fcca4c53ead32962ebfab6c858eb48b83
kureksebastian <kureksebastian@outlook.com>
2022-10-18 20:02:26 +0200

    Homework week 4

---
+++ Akademia.Net/Modulo/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.WriteLine("Podaj liczbę:");
+while (true)
+{
+    try
+    {
+        var number = GetInput();
+        Calculate(number);
+
+        if (Calculate(number))
+            Console.WriteLine("Liczba parzysta");
+        else
+            Console.WriteLine("Liczba nieparzysta");
+
+        Console.WriteLine("Podaj kolejną liczbę lub wpis exit aby wyjść z aplikacji.");
+    }
+
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+}
+
+static bool Calculate(int number)
+{
+    var result = number % 2;
+    if(result == 0)
+        return true;
+    else
+        return false;
+}
+
+ static int GetInput()
+{
+    var input = Console.ReadLine();
+    if (input.ToUpper() == "EXIT")
+        Environment.Exit(0);
+
+    if (!int.TryParse(input, out int number))
+        throw new Exception("Podana wartość jest niepoprawna");
+
+    return number;
+}
